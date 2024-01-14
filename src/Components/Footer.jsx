a04cb419b5384f7f6ad564e6e7020e302327e37f
6rahul9 <rahulmohature0421@gmail.com>
2024-01-14 14:08:15 +0530

    changed size

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,13 +3,13 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h3 className="footer__text">
+      <h2 className="footer__text">
         Designed and Built by{" "}
         <a href="https://github.com/6rahul9?tab=repositories" target="_blank">
           Rahul Mohuture
         </a>{" "}
-        with React, CSS, Chart.js, GeolocationAPI, and WeatherAPI.
-      </h3>
+        with React, CSS, OpenWeatherAPI and  GeolocationAPI.
+      </h2 >
     </div>
   );
 };
